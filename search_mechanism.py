874dd509a026b3ba1528d323d3176c7c59266695
Hymaneelirothu <142670415+Hymaneelirothu@users.noreply.github.com>
2024-09-19 08:39:46 -0700

    Update search_mechanism.py

--- search_mechanism.py
+++ search_mechanism.py
@@ -1,32 +1,79 @@
 import ast
 
-with open('sample_code.py', 'r') as file:
-    sample_code = file.read()
+class CodeAnalyzer(ast.NodeVisitor):
+    def __init__(self):
+        self.function_definitions = {}
+        self.data_type_usages = {}
 
-tree = ast.parse(sample_code)
-
-class GeneralSearchVisitor(ast.NodeVisitor):
-    def __init__(self, search_type, search_name=None):
-        self.search_type = search_type
-        self.search_name = search_name
-        self.results = []
-
-    def visit(self, node):
-        if isinstance(node, self.search_type):
-            if self.search_name:
-                if hasattr(node, 'id') and node.id == self.search_name:
-                    self.results.append((node.lineno, node.col_offset))
-            else:
-                self.results.append((node.lineno, node.col_offset))
+    def visit_FunctionDef(self, node):
+        # Store function definitions
+        self.function_definitions[node.name] = {
+            'lineno': node.lineno,
+            'col_offset': node.col_offset,
+            'args': [arg.arg for arg in node.args.args]
+        }
         self.generic_visit(node)
 
-def search_elements(search_type, search_name=None):
-    visitor = GeneralSearchVisitor(search_type, search_name)
-    visitor.visit(tree)
-    return visitor.results
+    def visit_Call(self, node):
+        # Record function calls
+        if isinstance(node.func, ast.Name):
+            func_name = node.func.id
+            if func_name not in self.function_definitions:
+                self.function_definitions[func_name] = {'calls': []}
+            self.function_definitions[func_name].setdefault('calls', []).append({
+                'lineno': node.lineno,
+                'col_offset': node.col_offset
+            })
+        self.generic_visit(node)
 
-function_call_results = search_elements(ast.Call)
-print("\nFunction call occurrences:", function_call_results)
+    def visit_AnnAssign(self, node):
+        # Record data type annotations
+        if isinstance(node.annotation, ast.Name):
+            data_type = node.annotation.id
+            if data_type not in self.data_type_usages:
+                self.data_type_usages[data_type] = []
+            self.data_type_usages[data_type].append({
+                'lineno': node.lineno,
+                'col_offset': node.col_offset
+            })
+        self.generic_visit(node)
 
-data_type_results = search_elements(ast.Constant)
-print("\nOccurrences of constants:", data_type_results)
+    def visit_Name(self, node):
+        # Record data type usages (e.g., variables)
+        if isinstance(node.ctx, ast.Load):
+            # Just a simple example of usage
+            if node.id not in self.data_type_usages:
+                self.data_type_usages[node.id] = []
+            self.data_type_usages[node.id].append({
+                'lineno': node.lineno,
+                'col_offset': node.col_offset
+            })
+        self.generic_visit(node)
+
+def analyze_code(code):
+    tree = ast.parse(code)
+    analyzer = CodeAnalyzer()
+    analyzer.visit(tree)
+    return analyzer.function_definitions, analyzer.data_type_usages
+
+def search_for_function(func_name, function_definitions):
+    return function_definitions.get(func_name, 'Function not found')
+
+def search_for_data_type(data_type, data_type_usages):
+    return data_type_usages.get(data_type, 'Data type not used')
+
+# Load sample code
+with open('sample_code.py', 'r') as file:
+    code = file.read()
+
+# Analyze the code
+function_definitions, data_type_usages = analyze_code(code)
+
+# Example searches
+print("Function Definitions:")
+for func_name, details in function_definitions.items():
+    print(f"Function '{func_name}': Defined at line {details.get('lineno')}")
+
+print("\nData Type Usages:")
+for data_type, locations in data_type_usages.items():
+    print(f"Data Type '{data_type}': Used at {locations}")
